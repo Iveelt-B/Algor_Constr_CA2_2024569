dd4f1a88477fc3474559f9b4a9a212df06035ce1
Iveelt-B <Iveeelt@gmail.com>
2025-04-25 15:21:11 +0100

    3. I implemented a sorting algorithm using Insertion Sort and wrote the text to display the menu.

--- src/algor_constr_ca2_2024569/Algor_Constr_CA2_2024569.java
+++ src/algor_constr_ca2_2024569/Algor_Constr_CA2_2024569.java
@@ -4,6 +4,8 @@
  */
 package algor_constr_ca2_2024569;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author iveelteddie
@@ -14,7 +16,27 @@ public class Algor_Constr_CA2_2024569 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        private ArrayList<Employee> employees = new ArrayList<>();
+        private ArrayList<Department> departments = new ArrayList<>();
+        
+        // Preparing 4 departments.
+        public Algor_Constr_CA2_2024569() {
+            departments.add(new Department(DepartmentType.IT, 1));
+            departments.add(new Department(DepartmentType.HR, 2));
+            departments.add(new Department(DepartmentType.MARKETING, 3));
+            departments.add(new Department(DepartmentType.FINANCE, 4));
+        }
+        
+        // Show the menu.
+        public void displayMenu() {
+            System.out.println("Please select an option:");
+            System.out.println("1. Sort of all employees");
+            System.out.println("2. Search for employees");
+            System.out.println("3. Add Employee");
+            System.out.println("4. Random Employee");
+            System.out.println("5. Exit");
+            
+        }
     }
     
 }
--- src/algor_constr_ca2_2024569/SortingAlgorithm.java
+++ src/algor_constr_ca2_2024569/SortingAlgorithm.java
@@ -4,10 +4,24 @@
  */
 package algor_constr_ca2_2024569;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author iveelteddie
  */
 public class SortingAlgorithm {
     
+    public static void insertonSort(ArrayList<String> list) {
+        for (int i = 1; i < list.size(); i++) {
+            String key = list.get(i);
+            int j = i - 1;
+            
+            while (j >= 0 && list.get(j).compareTo(key) > 0) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, key);
+        }
+    }
 }
